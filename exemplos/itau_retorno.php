ca5723349a619d7440eba9ac1c79b833dd868113
Eduardo Gusmão <eduguscontra3@hotmail.com>
2016-10-20 14:42:47 -0200

    exemplo itau

---
+++ exemplos/itau_retorno.php
@@ -0,0 +1,7 @@
+<?php
+include realpath(__DIR__ . '/../../../') . DIRECTORY_SEPARATOR . 'autoload.php';
+$retorno = \Eduardokum\LaravelBoleto\Cnab\Retorno\Factory::make(__DIR__ . DIRECTORY_SEPARATOR . 'arquivos' . DIRECTORY_SEPARATOR . 'itau.ret');
+$retorno->processar();
+
+echo $retorno->getBancoNome();
+dd($retorno->getDetalhes());
