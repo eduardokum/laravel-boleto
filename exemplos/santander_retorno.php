13ba34d40e78fffc501df57736d6072d727d9fd5
Eduardo Gusmão <eduguscontra3@hotmail.com>
2016-10-20 14:30:53 -0200

    exemplo santander

--- exemplos/santander_retorno.php
+++ exemplos/santander_retorno.php
@@ -1,6 +1,6 @@
 <?php
 include realpath(__DIR__ . '/../../../') . DIRECTORY_SEPARATOR . 'autoload.php';
-$retorno = \Eduardokum\LaravelBoleto\Cnab\Retorno\Factory::make(__DIR__ . DIRECTORY_SEPARATOR . 'arquivos' . DIRECTORY_SEPARATOR . 'cef.ret');
+$retorno = \Eduardokum\LaravelBoleto\Cnab\Retorno\Factory::make(__DIR__ . DIRECTORY_SEPARATOR . 'arquivos' . DIRECTORY_SEPARATOR . 'santander.ret');
 $retorno->processar();
 
 echo $retorno->getBancoNome();
