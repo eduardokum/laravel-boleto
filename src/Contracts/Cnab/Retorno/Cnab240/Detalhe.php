c1c5050af727733123aed5345ec701411d1a9454
Eduardo Gusmão <eduguscontra3@hotmail.com>
2017-01-09 10:20:34 -0200

    Detalhe contract

--- src/Contracts/Cnab/Retorno/Cnab240/Detalhe.php
+++ src/Contracts/Cnab/Retorno/Cnab240/Detalhe.php
@@ -2,16 +2,8 @@
 
 namespace Eduardokum\LaravelBoleto\Contracts\Cnab\Retorno\Cnab240;
 
+use Eduardokum\LaravelBoleto\Contracts\Cnab\Retorno\Detalhe as DetalheBase;
 
-interface Detalhe
+interface Detalhe extends DetalheBase
 {
-
-    const OCORRENCIA_LIQUIDADA = 1;
-    const OCORRENCIA_BAIXADA = 2;
-    const OCORRENCIA_ENTRADA = 3;
-    const OCORRENCIA_ALTERACAO = 4;
-    const OCORRENCIA_PROTESTADA = 5;
-    const OCORRENCIA_OUTROS = 6;
-    const OCORRENCIA_ERRO = 9;
-
 }
