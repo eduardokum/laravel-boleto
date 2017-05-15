eedcff2e3da4e4e031eb2a266fa5dfdf86857aa0
Eduardo Gusmão <eduguscontra3@hotmail.com>
2017-05-15 10:08:21 -0300

    data desconto zerada todos os bancos

--- src/Cnab/Remessa/Cnab400/Banco/Bancoob.php
+++ src/Cnab/Remessa/Cnab400/Banco/Bancoob.php
@@ -188,7 +188,7 @@ class Bancoob extends AbstractRemessa implements RemessaContract
         $this->add(161, 166, Util::formatCnab('9', 0, 6, 4));
         $this->add(167, 172, Util::formatCnab('9', $juros, 6, 4));
         $this->add(173, 173, '2'); //Tipo de distribuição: 1 - Cooperativa 2 - Cliente
-        $this->add(174, 179, $boleto->getDataDesconto()->format('dmy'));
+        $this->add(174, 179, $boleto->getDesconto() > 0 ? $boleto->getDataDesconto()->format('dmy') : '000000');
         $this->add(180, 192, Util::formatCnab('9', $boleto->getDesconto(), 13, 2));
         $this->add(193, 193, '9');
         $this->add(194, 205, Util::formatCnab('9', 0, 12, 2));
