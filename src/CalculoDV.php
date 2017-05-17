58aa1f56d9c5ea8863ca0ba2873e09b4b0030710
Eduardo Gusmão <eduguscontra3@hotmail.com>
2017-05-17 09:38:09 -0300

    novo calculo sicredi e banrisul

--- src/CalculoDV.php
+++ src/CalculoDV.php
@@ -1,7 +1,6 @@
 <?php
 namespace Eduardokum\LaravelBoleto;
 
-
 class CalculoDV
 {
 
@@ -10,15 +9,18 @@ class CalculoDV
     | 001 - Banco do Brasil
     |--------------------------------------------------------------------------
     */
-    public static function bbAgencia($agencia) {
+    public static function bbAgencia($agencia)
+    {
         return Util::modulo11($agencia, 2, 9, 0, 'X');
     }
 
-    public static function bbContaCorrente($contaCorrente) {
+    public static function bbContaCorrente($contaCorrente)
+    {
         return Util::modulo11($contaCorrente, 2, 9, 0, 'X');
     }
 
-    public static function bbNossoNumero($nossoNumero) {
+    public static function bbNossoNumero($nossoNumero)
+    {
         return strlen($nossoNumero) >= 17 ? null : Util::modulo11($nossoNumero);
     }
 
@@ -27,11 +29,13 @@ class CalculoDV
     | 004 - Banco do Nordeste
     |--------------------------------------------------------------------------
     */
-    public static function bnbAgencia($agencia) {
+    public static function bnbAgencia($agencia)
+    {
         return Util::modulo11($agencia, 2, 9, 0, 'X');
     }
 
-    public static function bnbNossoNumero($nossoNumero) {
+    public static function bnbNossoNumero($nossoNumero)
+    {
         return Util::modulo11(Util::numberFormatGeral($nossoNumero, 7));
     }
 
@@ -40,7 +44,8 @@ class CalculoDV
     | 033 - Santander
     |--------------------------------------------------------------------------
     */
-    public static function santanderContaCorrente($agencia, $contaCorrente) {
+    public static function santanderContaCorrente($agencia, $contaCorrente)
+    {
         $n = Util::numberFormatGeral($agencia, 4)
             . '00'
             . Util::numberFormatGeral($contaCorrente, 8);
@@ -54,7 +59,8 @@ class CalculoDV
         return $unidade == 0 ? $unidade : 10 - $unidade;
     }
 
-    public static function santanderNossoNumero($nossoNumero) {
+    public static function santanderNossoNumero($nossoNumero)
+    {
         return Util::modulo11($nossoNumero);
     }
 
@@ -63,18 +69,19 @@ class CalculoDV
     | 041 - Banrisul
     |--------------------------------------------------------------------------
     */
-    public static function banrisulAgencia($agencia) {
+    public static function banrisulAgencia($agencia)
+    {
         $newDv1 = $dv1 = Util::modulo10($agencia);
         $dv2 = Util::modulo11($agencia . $dv1, 2, 7);
 
-        if($dv2 == 1 && $dv1 != 9) {
+        if ($dv2 == 1 && $dv1 != 9) {
             $newDv1 = 1;
         }
-        if($dv2 == 1 && $dv1 == 9) {
+        if ($dv2 == 1 && $dv1 == 9) {
             $newDv1 = 0;
         }
 
-        if($dv1 != $newDv1) {
+        if ($dv1 != $newDv1) {
             $dv1 = $newDv1;
             $dv2 = Util::modulo11($agencia . $dv1, 2, 7);
         }
@@ -82,7 +89,8 @@ class CalculoDV
         return $dv1.$dv2;
     }
 
-    public static function banrisulContaCorrente($contaCorrente) {
+    public static function banrisulContaCorrente($contaCorrente)
+    {
         $chars = array_reverse(str_split($contaCorrente, 1));
         $sums = str_split('234567423', 1);
 
@@ -93,30 +101,31 @@ class CalculoDV
 
         $resto = $sum%11;
 
-        if($resto == 0) {
+        if ($resto == 0) {
             return $resto;
         }
 
-        if($resto == 1) {
+        if ($resto == 1) {
             return 6;
         }
 
         return 11 - $resto;
     }
 
-    public static function banrisulNossoNumero($nossoNumero) {
+    public static function banrisulNossoNumero($nossoNumero)
+    {
         return self::banrisulDuploDigito($nossoNumero);
     }
 
-    public static function banrisulDuploDigito($campo) {
+    public static function banrisulDuploDigito($campo)
+    {
         $dv1 = Util::modulo10($campo);
-        $dv2 = Util::modulo11($campo . $dv1, 2, 7);
-        if ($dv2 == 1) {
+        $dv2 = Util::modulo11($campo . $dv1, 2, 7, 0, 10);
+        if ($dv2 == 10) {
             $dv1++;
-            $dv2 = Util::modulo11($campo . $dv1, 2, 7);
+            $dv2 = Util::modulo11($campo . $dv1, 2, 7, 0, 10);
             if ($dv1 > 9) {
                 $dv1 = 0;
-                $dv2 = Util::modulo11($campo . $dv1, 2, 7);
             }
         }
         return $dv1 . $dv2;
@@ -127,13 +136,15 @@ class CalculoDV
     | 104 - Caixa Econômica Federal
     |--------------------------------------------------------------------------
     */
-    public static function cefContaCorrente($agencia, $contaCorrente) {
+    public static function cefContaCorrente($agencia, $contaCorrente)
+    {
         $n = Util::numberFormatGeral($agencia, 4)
             . Util::numberFormatGeral($contaCorrente, 11);
         return Util::modulo11($n);
     }
 
-    public static function cefNossoNumero($nossoNumero) {
+    public static function cefNossoNumero($nossoNumero)
+    {
         return Util::modulo11($nossoNumero);
     }
 
@@ -142,16 +153,19 @@ class CalculoDV
     | 237 - Bradesco
     |--------------------------------------------------------------------------
     */
-    public static function bradescoAgencia($agencia) {
+    public static function bradescoAgencia($agencia)
+    {
         $dv = Util::modulo11($agencia, 2, 9, 0, 'P');
         return $dv == 11 ? 0 : $dv;
     }
     
-    public static function bradescoContaCorrente($contaCorrente) {
+    public static function bradescoContaCorrente($contaCorrente)
+    {
         return Util::modulo11($contaCorrente, 2, 9, 0, 'P');
     }
 
-    public static function bradescoNossoNumero($carteira, $nossoNumero) {
+    public static function bradescoNossoNumero($carteira, $nossoNumero)
+    {
         return Util::modulo11($carteira . $nossoNumero, 2, 7, 0, 'P');
     }
 
@@ -160,13 +174,15 @@ class CalculoDV
     | 341 - Itau
     |--------------------------------------------------------------------------
     */
-    public static function itauContaCorrente($agencia, $contaCorrente) {
+    public static function itauContaCorrente($agencia, $contaCorrente)
+    {
         $n = Util::numberFormatGeral($agencia, 4)
             . Util::numberFormatGeral($contaCorrente, 5);
         return Util::modulo10($n);
     }
 
-    public static function itauNossoNumero($agencia, $conta, $carteira, $numero_boleto) {
+    public static function itauNossoNumero($agencia, $conta, $carteira, $numero_boleto)
+    {
         $n = Util::numberFormatGeral($agencia, 4)
             . Util::numberFormatGeral($conta, 5)
             . Util::numberFormatGeral($carteira, 3)
@@ -179,13 +195,14 @@ class CalculoDV
     | 748 - Sicredi
     |--------------------------------------------------------------------------
     */
-    public static function sicrediNossoNumero($agencia, $posto, $conta, $ano, $byte, $numero_boleto) {
+    public static function sicrediNossoNumero($agencia, $posto, $conta, $ano, $byte, $numero_boleto)
+    {
         $n = Util::numberFormatGeral($agencia, 4)
             . Util::numberFormatGeral($posto, 2)
             . Util::numberFormatGeral($conta, 5)
             . Util::numberFormatGeral($ano, 2)
             . Util::numberFormatGeral($byte, 1)
-            . Util::numberFormatGeral($byte, 5);
+            . Util::numberFormatGeral($numero_boleto, 5);
         return  Util::modulo11($n);
     }
 
@@ -194,11 +211,13 @@ class CalculoDV
     | 756 - Bancoob
     |--------------------------------------------------------------------------
     */
-    public static function bancoobAgencia($agencia) {
+    public static function bancoobAgencia($agencia)
+    {
         return Util::modulo11($agencia);
     }
 
-    public static function bancoobNossoNumero($agencia, $convenio, $numero_boleto) {
+    public static function bancoobNossoNumero($agencia, $convenio, $numero_boleto)
+    {
         $n = Util::numberFormatGeral($agencia, 4)
             . Util::numberFormatGeral($convenio, 10)
             . Util::numberFormatGeral($numero_boleto, 7);
