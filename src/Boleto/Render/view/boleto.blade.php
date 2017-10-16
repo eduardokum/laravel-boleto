8f0743622e0c68343c39fb8071b13b08e182fb80
Eduardo Gusmão <eduguscontra3@hotmail.com>
2017-10-16 10:26:16 -0200

    extract overwrite

--- src/Boleto/Render/view/boleto.blade.php
+++ src/Boleto/Render/view/boleto.blade.php
@@ -2,7 +2,7 @@
 @section('boleto')
 
     @foreach($boletos as $i => $boleto)
-        @php extract($boleto); @endphp
+        @php extract($boleto, EXTR_OVERWRITE); @endphp
         @if($mostrar_instrucoes)
             <div class="noprint info">
                 <h2>Instruções de Impressão</h2>
