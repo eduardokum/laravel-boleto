ebe2fe6ab0d542311b497255c8d477f755dba0bb
Eduardo Gusmão <eduguscontra3@hotmail.com>
2018-01-04 16:26:11 -0200

    carteira 28 -> 26 bradesco remessa

--- src/Cnab/Remessa/Cnab240/Banco/Bradesco.php
+++ src/Cnab/Remessa/Cnab240/Banco/Bradesco.php
@@ -82,7 +82,7 @@ class Bradesco extends AbstractRemessa implements RemessaContract
      *
      * @var array
      */
-    protected $carteiras = ['09', '28'];
+    protected $carteiras = ['09', '26'];
 
     /**
      * Codigo do cliente junto ao banco.
