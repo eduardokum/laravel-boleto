e2f555f5b7b1d0f18fd942496bab87ea12803666
Guilherme Couto <guilherme-costa-couto@hotmail.com>
2018-07-11 17:48:31 -0300

    Melhorias para identificar o tipo do cliente automaticamente (2 cnpj e 1 cpf)

--- src/Cnab/Remessa/Cnab240/Banco/Santander.php
+++ src/Cnab/Remessa/Cnab240/Banco/Santander.php
@@ -184,7 +184,7 @@ class Santander extends AbstractRemessa implements RemessaContract
         if ($boleto->getStatus() == $boleto::STATUS_ALTERACAO) {
             $this->add(16, 17, self::OCORRENCIA_ALT_OUTROS_DADOS);
         }
-        $this->add(18, 18, Util::formatCnab('9', 1, 1));
+        $this->add(18, 18, strlen(Util::onlyNumbers($boleto->getPagador()->getDocumento())) == 14 ? 2 : 1);
         $this->add(19, 33, Util::formatCnab('9', Util::onlyNumbers($boleto->getPagador()->getDocumento()), 15));
         $this->add(34, 73, Util::formatCnab('X', $boleto->getPagador()->getNome(), 40));
         $this->add(74, 113, Util::formatCnab('X', $boleto->getPagador()->getEndereco(), 40));
