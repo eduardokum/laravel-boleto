d07358d4ee6d1e896d17a0f5eea5a7a39c548e9a
Karla dos Santos <karla.krs@outlook.com>
2018-07-13 08:25:23 -0300

    Correcao de codigo do protesto na remessa com CNAB240 BB.

--- src/Cnab/Remessa/Cnab240/Banco/Bb.php
+++ src/Cnab/Remessa/Cnab240/Banco/Bb.php
@@ -205,8 +205,10 @@ class Bb extends AbstractRemessa implements RemessaContract
         $this->add(166, 180, Util::formatCnab('9', 0, 15, 2));
         $this->add(181, 195, Util::formatCnab('9', 0, 15, 2));
         $this->add(196, 220, Util::formatCnab('X', $boleto->getNumeroControle(), 25));
-        $this->add(221, 221, self::PROTESTO_SEM);
-        $this->add(221, 221, $boleto->getDiasProtesto() > 0? self::PROTESTO_DIAS_UTEIS : 3);
+        $this->add(221, 221, self::PROTESTO_NAO_PROTESTAR);
+        if ($boleto->getDiasProtesto() > 0) {
+            $this->add(221, 221, self::PROTESTO_DIAS_UTEIS);
+        }
         $this->add(222, 223, Util::formatCnab('9', $boleto->getDiasProtesto(), 2));
         $this->add(224, 224, '0');
         $this->add(225, 227, '000');
