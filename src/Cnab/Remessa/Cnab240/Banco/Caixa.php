ffd9b0c2d7e1c62c328d01e4ca2a4858c47af4bb
Guilherme Couto <guilherme-costa-couto@hotmail.com>
2018-08-24 15:39:28 -0300

    Melhorias para utilizar o documento do pagador no Segmento Q - Posicao 18

--- src/Cnab/Remessa/Cnab240/Banco/Caixa.php
+++ src/Cnab/Remessa/Cnab240/Banco/Caixa.php
@@ -209,7 +209,7 @@ class Caixa extends AbstractRemessa implements RemessaContract
         if ($boleto->getStatus() == $boleto::STATUS_CUSTOM) {
             $this->add(16, 17, sprintf('%2.02s', $boleto->getComando()));
         }
-        $this->add(18, 18, strlen(Util::onlyNumbers($this->getPagador()->getDocumento())) == 14 ? 2 : 1);
+        $this->add(18, 18, strlen(Util::onlyNumbers($boleto->getPagador()->getDocumento())) == 14 ? 2 : 1);
         $this->add(19, 33, Util::formatCnab('9', Util::onlyNumbers($boleto->getPagador()->getDocumento()), 15));
         $this->add(34, 73, Util::formatCnab('X', $boleto->getPagador()->getNome(), 40));
         $this->add(74, 113, Util::formatCnab('X', $boleto->getPagador()->getEndereco(), 40));
