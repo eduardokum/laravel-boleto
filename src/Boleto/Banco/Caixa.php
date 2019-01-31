e44c09b42b6a76748859b952d089ce416c44e75e
joaogarupa <37148294+joaogarupa@users.noreply.github.com>
2019-01-31 15:32:00 -0200

    Atualiza código de espécies conforme documentação

--- src/Boleto/Banco/Caixa.php
+++ src/Boleto/Banco/Caixa.php
@@ -32,11 +32,34 @@ class Caixa  extends AbstractBoleto implements BoletoContract
      * @var string
      */
     protected $especiesCodigo = [
-        'DM' => '01',
-        'NP' => '02',
-        'DS' => '03',
-        'NS' => '05',
-        'LC' => '06',
+        'CH'  => '01',  //Cheque
+        'DM'  => '02',  //Duplicata Mercantil
+        'DMI' => '03',  //Duplicata Mercantil p/ Indicação
+        'DS'  => '04',  //Duplicata de Serviço
+        'DSI' => '05',  //Duplicata de Serviço p/ Indicação
+        'DR'  => '06',  //Duplicata Rural
+        'LC'  => '07',  //Letra de Câmbio
+        'NCC' => '08',  //Nota de Crédito Comercial
+        'NCE' => '09',  //Nota de Crédito à Exportação
+        'NCI' => '10',  //Nota de Crédito Industrial
+        'NCR' => '11',  //Nota de Crédito Rural
+        'NP'  => '12',  //Nota Promissória
+        'NPR' => '13',  //Nota Promissória Rural
+        'TM'  => '14',  //Triplicata Mercantil
+        'TS'  => '15',  //Triplicata de Serviço
+        'NS'  => '16',  //Nota de Seguro
+        'RC'  => '17',  //Recibo
+        'FAT' => '18',  //Fatura
+        'ND'  => '19',  //Nota de Débito
+        'AP'  => '20',  //Apólice de Seguro
+        'ME'  => '21',  //Mensalidade Escolar
+        'PC'  => '22',  //Parcela de Consórcio
+        'NF'  => '23',  //Nota Fiscal
+        'DD'  => '24',  //Documento de Dívida
+        'CPR' => '25',  //Cédula de Produto Rural
+        'CC'  => '31',  //Cartão de Crédito
+        'BP'  => '32',  //Boleto Proposta
+        'OU'  => '99'   //Outros
     ];
     /**
      * Codigo do cliente junto ao banco.
