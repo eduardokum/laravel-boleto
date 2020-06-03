349b2cb7e07b8d306a64d40c1330a010f28ff1a0
Lucas Marques <lucasmarks1991@hotmail.com>
2020-06-02 21:40:45 -0300

    espécie de título bradesco ajustado para cnab 240 e 400

--- src/Boleto/Banco/Bradesco.php
+++ src/Boleto/Banco/Bradesco.php
@@ -44,40 +44,58 @@ class Bradesco  extends AbstractBoleto implements BoletoContract
      *
      * @var string
      */
-    protected $especiesCodigo = [
-        'CH'  => '01', //Cheque
-        'DM'  => '02', //Duplicata Mercantil
-        'DMI' => '03', //Duplicata Mercantil p/ Indicação
-        'DS'  => '04', //Duplicata de Serviço
-        'DSI' => '05', //Duplicata de Serviço p/ Indicação
-        'DR'  => '06', //Duplicata Rural
-        'LC'  => '07', //Letra de Câmbio
-        'NCC' => '08', //Nota de Crédito Comercial
-        'NCE' => '09', //Nota de Crédito a Exportação
-        'NCI' => '10', //Nota de Crédito Industrial
-        'NCR' => '11', //Nota de Crédito Rural
-        'NP'  => '12', //Nota Promissória
-        'NPR' => '13', //Nota Promissória Rural
-        'TM'  => '14', //Triplicata Mercantil
-        'TS'  => '15', //Triplicata de Serviço
-        'NS'  => '16', //Nota de Seguro
-        'RC'  => '17', //Recibo
-        'FAT' => '18', //Fatura
-        'ND'  => '19', //Nota de Débito
-        'AP'  => '20', //Apólice de Seguro
-        'ME'  => '21', //Mensalidade Escolar
-        'PC'  => '22', //Parcela de Consórcio
-        'NF'  => '23', //Nota Fiscal
-        'DD'  => '24', //Documento de Dívida
-        'CPR' => '25', //Cédula de Produto Rural,
-        'WAR' => '26', //Warrant
-        'DAE' => '27', //Dívida Ativa do Estado
-        'DAM' => '28', //Dívida Ativa do Município
-        'DAU' => '29', //Dívida Ativa da União
-        'EC'  => '30', //Encargos condominiais
-        'CC'  => '31', //CC Cartão de Crédito,
-        'BDP' => '32', //BDP - Boleto de Proposta
-        'O'   => '99', //Outros,
+    protected $especiesCodigo240 = [
+        'CH'  => '01', // Cheque
+        'DM'  => '02', // Duplicata Mercantil
+        'DMI' => '03', // Duplicata Mercantil p/ Indicação
+        'DS'  => '04', // Duplicata de Serviço
+        'DSI' => '05', // Duplicata de Serviço p/ Indicação
+        'DR'  => '06', // Duplicata Rural
+        'LC'  => '07', // Letra de Câmbio
+        'NCC' => '08', // Nota de Crédito Comercial
+        'NCE' => '09', // Nota de Crédito a Exportação
+        'NCI' => '10', // Nota de Crédito Industrial
+        'NCR' => '11', // Nota de Crédito Rural
+        'NP'  => '12', // Nota Promissória
+        'NPR' => '13', // Nota Promissória Rural
+        'TM'  => '14', // Triplicata Mercantil
+        'TS'  => '15', // Triplicata de Serviço
+        'NS'  => '16', // Nota de Seguro
+        'RC'  => '17', // Recibo
+        'FAT' => '18', // Fatura
+        'ND'  => '19', // Nota de Débito
+        'AP'  => '20', // Apólice de Seguro
+        'ME'  => '21', // Mensalidade Escolar
+        'PC'  => '22', // Parcela de Consórcio
+        'NF'  => '23', // Nota Fiscal
+        'DD'  => '24', // Documento de Dívida
+        'CPR' => '25', // Cédula de Produto Rural
+        'WAR' => '26', // Warrant
+        'DAE' => '27', // Dívida Ativa do Estado
+        'DAM' => '28', // Dívida Ativa do Município
+        'DAU' => '29', // Dívida Ativa da União
+        'EC'  => '30', // Encargos condominiais
+        'CC'  => '31', // Cartão de Crédito
+        'BDP' => '32', // Boleto de Proposta
+        'O'   => '99', // Outros
+    ];
+    /**
+     * Espécie do documento, coódigo para remessa
+     *
+     * @var string
+     */
+    protected $especiesCodigo400 = [
+        'DM'  => '01', // Duplicata Mercantil
+        'NP'  => '02', // Nota Promissória
+        'NS'  => '03', // Nota de Seguro
+        'CS'  => '04', // Cobrança Seriada
+        'RC'  => '05', // Recibo
+        'LC'  => '10', // Letra de Câmbio
+        'ND'  => '11', // Nota de Débito
+        'DS'  => '12', // Duplicata de Serviço
+        'CC'  => '31', // Cartão de Crédito
+        'BDP' => '32', // Boleto de Proposta
+        'O'   => '99', // Outros
     ];
     /**
      * Mostrar o endereço do beneficiário abaixo da razão e CNPJ na ficha de compensação
