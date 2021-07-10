83751b0692adefbb7cecc80121b6f237959dc815
Manito <eumanito@gmail.com>
2021-07-10 10:20:36 -0300

    progresso unicred

--- src/Cnab/Retorno/Cnab400/Banco/Bradesco.php
+++ src/Cnab/Retorno/Cnab400/Banco/Bradesco.php
@@ -147,7 +147,7 @@ class Bradesco extends AbstractRetorno implements RetornoCnab400
 
         $d = $this->detalheAtual();
         $d->setCarteira($this->rem(108, 108, $detalhe))
-            ->setNossoNumero($this->rem(71, 82, $detalhe))
+            ->setNossoNumero($this->rem(71, 81, $detalhe))
             ->setNumeroDocumento($this->rem(117, 126, $detalhe))
             ->setNumeroControle($this->rem(38, 62, $detalhe))
             ->setOcorrencia($this->rem(109, 110, $detalhe))
