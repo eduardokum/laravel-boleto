fcb1a1aa6a06190c10afc95addf3ef3654400dfc
elisiara <elisiarapodgorski@gmail.com>
2022-05-12 15:38:58 -0300

    feat: adiciona Unicred a lista de bancos

--- src/Util.php
+++ src/Util.php
@@ -985,6 +985,7 @@ final class Util
             BoletoContract::COD_BANCO_BANRISUL => 'Banco\\Banrisul',
             BoletoContract::COD_BANCO_BANCOOB => 'Banco\\Bancoob',
             BoletoContract::COD_BANCO_BNB => 'Banco\\Bnb',
+            BoletoContract::COD_BANCO_UNICRED => 'Banco\\Unicred',
         ];
 
         if (array_key_exists($banco, $aBancos)) {
