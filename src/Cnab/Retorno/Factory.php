2cdb6e5fe1781b634f098a9b09e3322bd41b9aad
elisiara <elisiarapodgorski@gmail.com>
2022-05-12 15:37:42 -0300

    feat: adiciona Unicred a lista de bancos

--- src/Cnab/Retorno/Factory.php
+++ src/Cnab/Retorno/Factory.php
@@ -56,6 +56,7 @@ class Factory
             BoletoContract::COD_BANCO_BANRISUL => 'Banco\\Banrisul',
             BoletoContract::COD_BANCO_BANCOOB => 'Banco\\Bancoob',
             BoletoContract::COD_BANCO_BNB => 'Banco\\Bnb',
+            BoletoContract::COD_BANCO_UNICRED => 'Banco\\Unicred',
         ];
 
         if (array_key_exists($banco, $aBancos)) {
