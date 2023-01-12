1418f8408dfc6151780299d75a3aa0805591bfb8
Eduardo Gusmão <eduguscontra3@hotmail.com>
2023-01-12 15:26:32 -0300

    pine e fibra

--- exemplos/fibra_remessa.php
+++ exemplos/fibra_remessa.php
@@ -2,31 +2,31 @@
 require 'autoload.php';
 $beneficiario = new \Eduardokum\LaravelBoleto\Pessoa(
     [
-        'nome'      => 'KRONOS LTDA',
-        'endereco'  => 'Rua , 79',
-        'cep'       => '13988-000',
-        'uf'        => 'SP',
-        'cidade'    => 'Itapira',
-        'documento' => '26.589.893/0001-46',
+        'nome'      => 'ACME',
+        'endereco'  => 'Rua um, 123',
+        'cep'       => '99999-999',
+        'uf'        => 'UF',
+        'cidade'    => 'CIDADE',
+        'documento' => '99.999.999/9999-99',
     ]
 );
 
 $pagador = new \Eduardokum\LaravelBoleto\Pessoa(
     [
-        'nome'      => 'Intrasis',
-        'endereco'  => 'Av. Engenheiro Caetano Alvares, 3536',
-        'bairro'    => 'Limão',
-        'cep'       => '02546-000',
-        'uf'        => 'SP',
-        'cidade'    => 'São Paulo',
-        'documento' => '10.228.511/0001-19',
+        'nome'      => 'Cliente',
+        'endereco'  => 'Rua um, 123',
+        'bairro'    => 'Bairro',
+        'cep'       => '99999-999',
+        'uf'        => 'UF',
+        'cidade'    => 'CIDADE',
+        'documento' => '999.999.999-99',
     ]
 );
 
 $boleto = new Eduardokum\LaravelBoleto\Boleto\Banco\Fibra(
     [
         'logo'                   => realpath(__DIR__ . '/../logos/') . DIRECTORY_SEPARATOR . '224.png',
-        'dataVencimento'         => (new \Carbon\Carbon())->addDays(15),
+        'dataVencimento'         => new \Carbon\Carbon('2023-01-04'),
         'valor'                  => 10,
         'multa'                  => false,
         'juros'                  => false,
@@ -35,27 +35,26 @@ $boleto = new Eduardokum\LaravelBoleto\Boleto\Banco\Fibra(
         'range'                  => 0,
         'pagador'                => $pagador,
         'beneficiario'           => $beneficiario,
-        'modalidadeCarteira'     => '5',
+        'modalidadeCarteira'     => 'D',
         'carteira'               => 112,
         'agencia'                => '0001',
-        'codigoCliente'          => '89191',
-        'conta'                  => '6713030',
+        'codigoCliente'          => '12345',
+        'conta'                  => '1234567',
         'descricaoDemonstrativo' => ['demonstrativo 1', 'demonstrativo 2', 'demonstrativo 3'],
         'instrucoes'             => ['instrucao 1', 'instrucao 2', 'instrucao 3'],
         'aceite'                 => 'N',
         'especieDoc'             => 'DM',
-        'chaveNfe'               => '35230126589893000146550010000124251745502439'
     ]
 );
 
 $remessa = new \Eduardokum\LaravelBoleto\Cnab\Remessa\Cnab400\Banco\Fibra(
     [
         'agencia'      => '0001',
-        'conta'        => '6713030',
+        'conta'        => '1234567',
         'contaDv'      => 9,
         'carteira'     => 112,
         'beneficiario' => $beneficiario,
-        'codigoCliente' => '89191',
+        'codigoCliente' => '12345',
     ]
 );
 $remessa->addBoleto($boleto);
