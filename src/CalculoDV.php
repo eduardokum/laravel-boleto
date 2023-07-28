a55717d284798b995c5a52bebb1a1a6ea93c21dc
Guilherme-Numerama <guilherme@numerama.com.br>
2023-07-28 09:09:56 -0300

    Cresol Remessa e retorno CNAB400

--- src/CalculoDV.php
+++ src/CalculoDV.php
@@ -197,6 +197,21 @@ class CalculoDV
     {
         return Util::modulo11($nossoNumero);
     }
+
+    /*
+    |--------------------------------------------------------------------------
+    | 133 - Cresol
+    |--------------------------------------------------------------------------
+    */    
+    public static function cresolContaCorrente($conta)
+    {
+        return Util::modulo11($conta, 2, 9, 0, 'P');
+    }
+
+    public static function cresolNossoNumero($carteira, $nossoNumero)
+    {
+        return Util::modulo11($carteira . Util::numberFormatGeral($nossoNumero, 11), 2, 7, 0, 'P');
+    }
 	
     /*
     |--------------------------------------------------------------------------
