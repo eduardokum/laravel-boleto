c01ff317daad27d1bcf7351b0723b0102262b7ae
Eduardo Gusmão <eduguscontra3@hotmail.com>
2023-09-08 11:20:15 +0100

    tests

--- tests/Remessa/RemessaCnab400Test.php
+++ tests/Remessa/RemessaCnab400Test.php
@@ -720,6 +720,51 @@ class RemessaCnab400Test extends TestCase
         $this->assertFileExists($file2);
     }
 
+    public function testRemessaRendimentoCnab400()
+    {
+        $boleto = new Boleto\Rendimento([
+            'logo' => realpath(__DIR__ . '/../../logos/') . DIRECTORY_SEPARATOR . '633.png',
+            'dataVencimento' => new \Carbon\Carbon(),
+            'valor' => 100,
+            'multa' => false,
+            'juros' => false,
+            'numero' => 2,
+            'numeroDocumento' => 2,
+            'pagador' => self::$pagador,
+            'beneficiario' => self::$beneficiario,
+            'carteira' => '121',
+            'agencia' => '0001',
+            'codigoCliente' => '5447390',
+            'conta' => '1234',
+            'modalidadeCarteira' => '6',
+            'descricaoDemonstrativo' => ['demonstrativo 1', 'demonstrativo 2', 'demonstrativo 3'],
+            'instrucoes' => ['instrucao 1', 'instrucao 2', 'instrucao 3'],
+            'aceite' => 'N',
+            'especieDoc' => 'DM',
+        ]);
+
+        $remessa = new Remessa\Rendimento([
+            'agencia'       => '0001',
+            'conta'         => '1234',
+            'contaDv'       => 9,
+            'carteira'      => 121,
+            'codigoCliente' => '5447390',
+            'beneficiario'  => self::$beneficiario,
+        ]);
+        $remessa->addBoleto($boleto);
+
+        $file = implode(DIRECTORY_SEPARATOR, [
+            __DIR__,
+            'files',
+            'cnab400',
+            'rendimento.txt',
+        ]);
+
+        $file2 = $remessa->save($file);
+
+        $this->assertFileExists($file2);
+    }
+
     public function testRemessaInterCnab400()
     {
         $boleto = new Boleto\Inter([
