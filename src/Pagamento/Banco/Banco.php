db96c19246ec8e1adadb5673d68d8911e6aa5646
Willian Mauricio <willian.mauricio@apresenta.me>
2025-09-23 11:39:44 -0300

    ajustando classe generica

--- src/Pagamento/Banco/Banco.php
+++ src/Pagamento/Banco/Banco.php
@@ -10,28 +10,5 @@ class Banco extends AbstractPagamento implements Pagamento
     public function __construct(array $params = [])
     {
         parent::__construct($params);
-        $this->setCamposObrigatorios('operacao');
-    }
-
-    protected $agencia = '0001';
-
-    /**
-     * Código do banco
-     *
-     * @var string
-     */
-    protected $codigoBanco = Pagamento::COD_BANCO_INTER;
-
-    /**
-     * @var string[]
-     */
-    protected $protectedFields = [];
-
-
-    public function setAgencia($agencia)
-    {
-        $this->agencia = $agencia;
-
-        return $this;
     }
 }
